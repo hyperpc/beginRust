31c71d0587a1916a761ce9faa80a23f00311873a
Robert <houtong_an@163.com>
2022-01-06 23:57:59 +0800

    ch01终端命令

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# beginRust
-Beginning Rust, from novice to professional
+# Rust实战
+
+## 从入门到精通
+
+### [官方教程](https://www.rust-lang.org/zh-CN/learn)
+
+![Cover](./IMG/cover.jpg)
---
+++ ch01_print/demo/demo.rs
@@ -0,0 +1,13 @@
+fn main(){
+    print!("Hello world!"); //字面量
+    
+    print!("{}, {}!", "Hello", "world"); //占位符{}
+
+    print!("{}\n{}!\n", "Hello", "world"); //\n
+
+    println!("Hello world!"); //println
+    
+    println!("Number: {}", 140); //140
+    println!("Number: {}", 00140); //00140
+
+}
